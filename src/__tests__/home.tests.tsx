525be738882ea40b671331c62caa1cd4c131b5b9
“malcolmmutare” <mutarem@gmail.com>
2024-04-02 12:47:39 +0200

    feat: ui responsiveness of all components on all devices

--- src/__tests__/home.tests.tsx
+++ src/__tests__/home.tests.tsx
@@ -1,18 +1,20 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Home from '../app/page';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Authentication from '../app/authentication/page';
 
-describe('Home component', () => {
-  test('renders the main heading', () => {
-    render(<Home />);
-    const mainHeading = screen.getByRole('heading', { name: /Today's Menu/i });
-    expect(mainHeading).toBeInTheDocument();
-  });
+test('renders authentication page with input field and submit button', () => {
+  render(<Authentication />);
+  const inputElement = screen.getByPlaceholderText('Enter your text');
+  expect(inputElement).toBeInTheDocument();
+  const submitButton = screen.getByText('Submit');
+  expect(submitButton).toBeInTheDocument();
+});
 
-  test('renders the documentation link', () => {
-    render(<Home />);
-    const docsLink = screen.getByRole('link', { name: /Docs/i });
-    expect(docsLink).toBeInTheDocument();
-    expect(docsLink).toHaveAttribute('href', 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app');
-  });
+test('submits form when valid text is entered', () => {
+  render(<Authentication />);
+  const inputElement = screen.getByPlaceholderText('Enter your text');
+  fireEvent.change(inputElement, { target: { value: 'Valid text' } });
+  fireEvent.submit(screen.getByRole('button', { name: 'Submit' }));
+  const successMessage = screen.getByText('Text submitted! Thank You');
+  expect(successMessage).toBeInTheDocument();
 });
