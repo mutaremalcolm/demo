f17615615e04ea5c460c64175795cb737f095d2c
“malcolmmutare” <mutarem@gmail.com>
2024-04-15 17:14:46 +0200

    file structure changes

---
+++ src/app/useState/useState.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UseState = () => {
+  return (
+    <>
+      <div>
+        <div>
+            This is the uState Page
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UseState;
