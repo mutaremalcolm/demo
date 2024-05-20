257c206d1405ca6fa7a844fad586a0b829c3c80e
“malcolmmutare” <mutarem@gmail.com>
2024-05-20 11:31:24 +0200

    Add Read More Component to Card Component in Blog Section
    - Implemented a Read More component within the card component for the blog section.
    - Enhanced user experience by allowing expansion of blog content.

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardHeader } from "./ui/card";
+import { ReadMore } from "./ReadMore";
 
 
 
@@ -15,8 +16,7 @@ const BlogCard = ({ project}: Props)=> {
             <CardHeader className="p-0">
                 {/* { image } */}
                 <div className="relative w-full h-[300px] flex items-center justify-center
-                bg-sky-600 dark:bg-secondary/40 xl:bg-assets-blog-react19  
-                xl:bg-[110%] xl:bg-no-repeat overflow-hidden"
+                bg-sky-600 xl:bg-assets-blog-react19 xl:bg-[110%] xl:bg-no-repeat overflow-hidden"
                 >
                     <Image 
                     className="absolute bottom-0 shadow-2xl m-2 mt-2" 
@@ -29,11 +29,8 @@ const BlogCard = ({ project}: Props)=> {
                 </div>
             </CardHeader>
             <div className="h-full px-8 py-6 text-black">
-                {/* <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">
-                {project.category}
-                </Badge> */}
                 <h4 className="h4 mb-1">{project.name}</h4>
-                <p className="text-muted-foreground text-lg">{project.description}</p>
+                <ReadMore id="read-more-text" text={project.description}/>            
             </div>
         </Card>
 );
