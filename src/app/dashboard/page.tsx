ba7b9c024baa3a52b6f591c94f5a4c80ca8417d3
Malcolm Mutare <135226305+mutaremalcolm@users.noreply.github.com>
2024-05-22 21:17:02 +0200

    # Pull Request: Adjust Dashboard Styles (#26)
    * Refactor Authentication Page and Add Typewriter Effect to Landing Page
    
    - Refactored the authentication page for improved performance and readability.
    - Added a typewriter effect to the landing page to enhance user engagement.
    
    * Add Footer Component to React Application
    
    - Implemented a new footer component to improve the application's layout and navigation.
    
    * Add Read More Component to Card Component in Blog Section
    
    - Implemented a Read More component within the card component for the blog section.
    - Enhanced user experience by allowing expansion of blog content.
    
    * feat: Add typewriter effect to hero section and refactor navbar
    
    - Implemented a typewriter effect in the hero section for dynamic text display.
    - Refactored the navbar for improved structure and responsiveness.
    - TODO: Implement a proper hero image to enhance visual appeal.
    
    * feat(tests): convert all tests to use userEvent for interactions
    
    - Updated tests in Authentication, Dashboard, and UseEffectDemo components to use userEvent instead of fireEvent for better simulation of user interactions.
    - Ensured tests are wrapped in act() where necessary to handle state updates correctly.
    - Improved readability and maintainability of test code by adopting userEvent across all components.
    
    * Refactor: Implement Global Styling for Cleaner Minimal Feel
    
    * feat: added boilerplate for the dashboard component
    
    * feat: adjusted styling for dashboard to match application styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,11 +12,11 @@ function Dashboard() {
         title="DashBoard Page"
         subtitle="This page demonstates how Navigation in React works"
         /> */}
-        <div className="bg-orange-100 min-h-screen">
+        <div className="bg-sky-600 min-h-screen">
             <div className="fixed bg-white text-blue-800 px-10 py-1 z-10 w-full">
                 <div className="flex items-center justify-between py-2 text-5xl">
                     <div className="font-bold text-blue-900 text-xl">
-                        React <span className="text-orange-600">Application</span>
+                        React <span className="text-blue-900">Application</span>
                     </div>
                     <div className="flex items-center text-gray-500">
                         <span className="material-icons-outlined p-2" style={{ fontSize: '30px' }}>search</span>
@@ -69,16 +69,16 @@ function Dashboard() {
 
                 <div className="w-10/12">
                     <div className="flex flex-row">
-                        <div className="bg-no-repeat bg-red-200 border border-red-300 rounded-xl w-7/12 mr-2 p-6"
+                        <div className="bg-no-repeat bg-sky-400 border border-sky-900 rounded-xl w-7/12 mr-2 p-6"
                             style={{ backgroundImage: 'url(https://previews.dropbox.com/p/thumb/AAvyFru8elv-S19NMGkQcztLLpDd6Y6VVVMqKhwISfNEpqV59iR5sJaPD4VTrz8ExV7WU9ryYPIUW8Gk2JmEm03OLBE2zAeQ3i7sjFx80O-7skVlsmlm0qRT0n7z9t07jU_E9KafA9l4rz68MsaZPazbDKBdcvEEEQPPc3TmZDsIhes1U-Z0YsH0uc2RSqEb0b83A1GNRo86e-8TbEoNqyX0gxBG-14Tawn0sZWLo5Iv96X-x10kVauME-Mc9HGS5G4h_26P2oHhiZ3SEgj6jW0KlEnsh2H_yTego0grbhdcN1Yjd_rLpyHUt5XhXHJwoqyJ_ylwvZD9-dRLgi_fM_7j/p.png?fv_content=true&size_mode=5)', backgroundPosition: '90% center' }}>
                             <p className="text-5xl text-indigo-900">Welcome <br /><strong>Lorem Ipsum</strong></p>
-                            <span className="bg-red-300 text-xl text-white inline-block rounded-full mt-12 px-8 py-2"><strong>01:51</strong></span>
+                            <span className="bg-sky-400 text-xl text-white inline-block rounded-full mt-12 px-8 py-2"><strong>01:51</strong></span>
                         </div>
 
-                        <div className="bg-no-repeat bg-orange-200 border border-orange-300 rounded-xl w-5/12 ml-2 p-6"
+                        <div className="bg-no-repeat bg-sky-600 border border-sky-900 rounded-xl w-5/12 ml-2 p-6"
                             style={{ backgroundImage: 'url(https://previews.dropbox.com/p/thumb/AAuwpqWfUgs9aC5lRoM_f-yi7OPV4txbpW1makBEj5l21sDbEGYsrC9sb6bwUFXTSsekeka5xb7_IHCdyM4p9XCUaoUjpaTSlKK99S_k4L5PIspjqKkiWoaUYiAeQIdnaUvZJlgAGVUEJoy-1PA9i6Jj0GHQTrF_h9MVEnCyPQ-kg4_p7kZ8Yk0TMTL7XDx4jGJFkz75geOdOklKT3GqY9U9JtxxvRRyo1Un8hOObbWQBS1eYE-MowAI5rNqHCE_e-44yXKY6AKJocLPXz_U4xp87K4mVGehFKC6dgk_i5Ur7gspuD7gRBDvd0sanJ9Ybr_6s2hZhrpad-2WFwWqSNkh/p.png?fv_content=true&size_mode=5)', backgroundPosition: '100% 40%' }}>
                             <p className="text-5xl text-indigo-900">Inbox <br /><strong>23</strong></p>
-                            <a href="#" className="bg-orange-300 text-xl text-white underline hover:no-underline inline-block rounded-full mt-12 px-8 py-2"><strong>See messages</strong></a>
+                            <a href="#" className="bg-sky-600 text-xl text-white underline hover:no-underline inline-block rounded-full mt-12 px-8 py-2"><strong>See messages</strong></a>
                         </div>
                     </div>
                     <div className="flex flex-row h-64 mt-6">
